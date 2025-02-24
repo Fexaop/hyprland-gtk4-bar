d95e807726deed6c4741e2d77e0ada222da64bc4
Gaurish Mehra <gaurishyt@gmail.com>
2025-02-24 18:59:21 +0530

    daemon

---
+++ test2.py
@@ -0,0 +1,10 @@
+import socket
+
+# Simple UDP receiver
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 6942))
+
+print("Listening for broadcasts on port 6942...")
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"Received from {addr}: {data.decode('utf-8')}")
